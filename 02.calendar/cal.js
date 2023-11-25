4d3bff6a7109f741cb0e7e6b642f400bce2dfc23
tuna24365 <bluefin24.365@gmail.com>
2023-11-25 13:34:21 +0900

    Refactor calendar script for enhanced readability and add HEADER_WIDTH constant.

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,50 +1,7 @@
 #!/usr/bin/env node
 import minimist from "minimist";
 
-const getFirstDateOfMonth = (year, month) => {
-  return new Date(year, month - 1, 1);
-};
-
-const getLastDateOfMonth = (year, month) => {
-  return new Date(year, month, 0);
-};
-
-const generateCalendarHeader = (year, month) => {
-  const monthName = getFirstDateOfMonth(year, month).toLocaleString("en-US", {
-    month: "short",
-  });
-  return `${monthName} ${year}`.padStart(14, " ");
-};
-
-const generateWeekHeader = () => {
-  return "Su Mo Tu We Th Fr Sa";
-};
-
-const generateDayFormat = (day) => {
-  const dayString = day.toString().padStart(2, " ");
-  return dayString + " ";
-};
-
-const generateBlankDays = (year, month) => {
-  return "   ".repeat(getFirstDateOfMonth(year, month).getDay());
-};
-
-const isSaturday = (year, month, day) => {
-  return new Date(year, month - 1, day).getDay() === 6;
-};
-
-const generateMonthDates = (year, month) => {
-  const dayInMonth = getLastDateOfMonth(year, month).getDate();
-  let monthDates = generateBlankDays(year, month);
-
-  for (let day = 1; day <= dayInMonth; day++) {
-    monthDates += generateDayFormat(day);
-    if (isSaturday(year, month, day)) {
-      monthDates += "\n";
-    }
-  }
-  return monthDates;
-};
+const HEADER_WIDTH = 14;
 
 const run = () => {
   const today = new Date();
@@ -71,6 +28,51 @@ const run = () => {
     generateWeekHeader(),
     generateMonthDates(year, month),
   ].join("\n");
+};
+
+const generateCalendarHeader = (year, month) => {
+  const monthName = getFirstDateOfMonth(year, month).toLocaleString("en-US", {
+    month: "short",
+  });
+  return `${monthName} ${year}`.padStart(HEADER_WIDTH, " ");
+};
+
+const getFirstDateOfMonth = (year, month) => {
+  return new Date(year, month - 1, 1);
+};
+
+const generateWeekHeader = () => {
+  return "Su Mo Tu We Th Fr Sa";
+};
+
+const generateMonthDates = (year, month) => {
+  const dayInMonth = getLastDateOfMonth(year, month).getDate();
+  let monthDates = generateBlankDays(year, month);
+
+  for (let day = 1; day <= dayInMonth; day++) {
+    monthDates += generateDayFormat(day);
+    if (isSaturday(year, month, day)) {
+      monthDates += "\n";
+    }
+  }
+  return monthDates;
+};;
+
+const getLastDateOfMonth = (year, month) => {
+  return new Date(year, month, 0);
+};
+
+const generateBlankDays = (year, month) => {
+  return "   ".repeat(getFirstDateOfMonth(year, month).getDay());
+};
+
+const generateDayFormat = (day) => {
+  const dayString = day.toString().padStart(2, " ");
+  return dayString + " ";
+};
+
+const isSaturday = (year, month, day) => {
+  return new Date(year, month - 1, day).getDay() === 6;
 };
 
 console.log(run());
